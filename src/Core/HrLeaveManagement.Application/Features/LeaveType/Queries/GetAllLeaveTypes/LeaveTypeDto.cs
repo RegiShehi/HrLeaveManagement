ddaf41087518deb6dbccdd5e9322c8845e4a19c7
Regi Shehi <regi_shehi@hotmail.com>
2024-11-28 17:05:00 +0100

    Add AutoMapper and MediatR

---
+++ src/Core/HrLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HrLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class LeaveTypeDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int DefaultDays { get; set; }
+}
---
+++ src/Core/HrLeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+using HrLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+namespace HrLeaveManagement.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+    }
+}
