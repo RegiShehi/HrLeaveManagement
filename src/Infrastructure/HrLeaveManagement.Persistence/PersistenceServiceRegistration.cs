2afb3ae845522e0b43677e583f9fadd5e09982dc
Regi Shehi <regi_shehi@hotmail.com>
2024-11-28 21:52:37 +0100

    Add Persistence Project

---
+++ src/Infrastructure/HrLeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HrLeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
