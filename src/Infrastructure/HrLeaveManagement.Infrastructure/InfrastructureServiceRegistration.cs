94c9cd74493caf283d7da56c70e4b84db454a100
Regi Shehi <regi_shehi@hotmail.com>
2024-11-29 14:20:56 +0100

    Add Infrastructure project

---
+++ src/Infrastructure/HrLeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HrLeaveManagement.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
